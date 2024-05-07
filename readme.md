aafb3579cfe5052224b7c22df6c3d50730d3c662
Paolo Damen <paolodamen@gmail.com>
2024-05-07 12:20:26 +0200

    init portfolio paolo

---
+++ readme.md
@@ -0,0 +1,66 @@
+# Paolo Damen - Full Stack Web Developer
+
+## Contact Information
+- Location: Madrid
+- Phone: +34 653 548 375
+- Email: paolodamen@gmail.com
+
+## Profile
+Self-motivated problem solver with extensive experience across various fields, including design, advertising, customer care, and administration. Proficient in multiple languages, with recent proficiency in Spanish. Adaptable, energetic, and detail-oriented, seeking new opportunities as a full-stack web developer.
+
+## Experience
+- **Transcription Specialist - Signum (Google)**, Madrid, 10-11-2022 / 19-04-2023
+- **Customer Care Specialist - Sitel (Jaguar Land Rover)**, Madrid, 08-11-2021 / 09-11-2022
+- **Technical Agent - Webhelp (Google Nest)**, Barcelona, 09-12-2020 / 20-04-2021
+- **Back Office Agent - Innovation Group**, Barcelona, 18-11-2019 / 01-09-2020
+- **Customer Care Agent - Newco (Scarlet)**, Barcelona, 29-07-2019 / 01-11-2019
+- **Customer Care Agent - Sixt (Drive Now)**, Berlin, 01-08-2018 / 01-11-2018
+- **Rider - Foodora**, Berlin, 01-11-2017 / 01-06-2018
+- **Photographer - Freelance**, Berlin, 01-10-2014 / 01-11-2018
+- **Editor - MOVIESTAR (Feature Movie)**, Berlin, 01-03-2014 / 01-09-2014
+- **Director / Writer / Editor - Freelance**, Los Angeles / Berlin, 01-07-2013 / 28-02-2013
+
+## Skills
+### Front-end
+- HTML5, CSS3, JavaScript (ES6+)
+- React, Bootstrap, Tailwind CSS
+
+### Back-end
+- Node.js, Express.js
+
+### Databases
+- MongoDB, MySQL
+
+### Version Control
+- Git
+
+### Other
+- Responsive Design
+- UI/UX Principles
+- RESTful APIs
+
+### Adobe CC
+- Photoshop, Illustrator, InDesign, After Effects, Premiere Pro
+
+### Other Tools
+- FinalCut Pro X
+- Logic Pro X
+
+## Languages
+- Dutch: Native
+- English: C2
+- German: C2
+- Spanish: C1
+- French: C1
+
+## Education
+- **Directing Certificate** - University of California Los Angeles, 01-09-2009 / 01-06-2012
+- **Cinematography Certificate** - University of California Los Angeles, 01-09-2009 / 01-01-2012
+- **Film Production Certificate** - University of California Los Angeles, 01-09-2008 / 01-06-2009
+- **Graphic Design Certificate** - University of California Los Angeles, 01-09-2005 / 01-06-2008
+- **Bachelor in Tax Law** - University of Amsterdam, 01-09-2000 / 01-05-2002
+- **Master in International Law** - University of Amsterdam, 01-09-1982 / 01-06-1988
+
+---
+
+Feel free to customize this README based on your preferences and additional details you'd like to include.
---
+++ script.js
@@ -0,0 +1,190 @@
+const header = document.querySelector('.header');
+const navMenu = document.querySelector('.nav');
+const navIcon = document.querySelector('.toggle-menu');
+const navLinks = document.querySelectorAll('.nav a');
+const navIndicators = document.querySelector('.hero__indicators');
+const dots = Array.from(navIndicators.children);
+const sections = document.querySelectorAll('section');
+const cards = document.querySelector('.cards');
+
+let current = '';
+
+// change header background color on scroll
+const showBg = () => {
+  const scroll = window.scrollY;
+
+  if (scroll > 100) {
+    header.classList.add('showBg');
+  } else {
+    header.classList.remove('showBg');
+  }
+};
+
+// function to show navigation
+const showNav = () => {
+  navMenu.classList.toggle('show');
+  navIcon.classList.toggle('active');
+  document.body.classList.toggle('fixed');
+};
+
+// nav links
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    const active = document.querySelector('.current');
+    active.classList.remove('current');
+    link.classList.add('current');
+
+    showNav();
+  });
+});
+
+// delete this line
+const hey = 'just testing out some stuff';
+
+const test = 'another test';
+
+// update the links
+const scrollUpdate = () => {
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop - sectionHeight / 5) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove('current');
+    if (link.classList.contains(current)) {
+      link.classList.add('current');
+    }
+  });
+
+  dots.forEach((dot) => {
+    dot.classList.remove('selected');
+    if (dot.classList.contains(current)) {
+      dot.classList.add('selected');
+    }
+  });
+};
+
+// Work section
+const cardImgs = document.querySelectorAll('.card__img img');
+
+cardImgs.forEach((img) => {
+  const card = img.closest('.card');
+  const imgHeight = img.offsetHeight;
+  const cardHeight = card.offsetHeight;
+
+  img.addEventListener('mouseenter', () => {
+    const percentageDifference = Math.abs(
+      ((imgHeight - cardHeight) / imgHeight) * 105
+    );
+    img.style.transform = `translateY(-${percentageDifference}%)`;
+  });
+
+  img.addEventListener('mouseleave', () => {
+    img.style.transform = 'translateY(0)';
+  });
+});
+
+// Blog section
+const getArticles = async () => {
+  const cards = document.querySelector('.cards');
+  const readMoreButton = document.getElementById('read-more-button');
+  const readLessButton = document.getElementById('read-less-button');
+
+  let articles = [];
+  let visibleArticles = 4;
+
+  const fetchMoreArticles = async () => {
+    try {
+      const response = await fetch(
+        'https://dev.to/api/articles?username=devlawrence'
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch articles');
+      }
+
+      articles = await response.json();
+
+      displayArticles();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const displayArticles = () => {
+    cards.innerHTML = '';
+
+    const articlesToShow = articles.slice(0, visibleArticles);
+    articlesToShow.forEach(
+      ({
+        cover_image,
+        title,
+        url,
+        description,
+        reading_time_minutes,
+        user,
+      }) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+        <img loading="lazy" src="${cover_image}" alt="" />
+        <div class="info">
+          <span class="time">${reading_time_minutes} min read</span>
+          <h5>${title}</h5>
+          <p>${description}</p>
+          <div class="d-flex">
+            <a target="_blank" href="${url}" class="btn card-btn">
+              <span>read</span>
+              <i class="fa-solid fa-up-right-from-square"></i>
+            </a>
+            <div>
+              <img class="profile-img" src="${user.profile_image}" alt="" />
+            </div>
+          </div>
+        </div>
+      `;
+
+        cards.appendChild(card);
+      }
+    );
+
+    if (visibleArticles >= articles.length) {
+      readMoreButton.classList.add('hide');
+      readLessButton.classList.add('show');
+    } else {
+      readMoreButton.classList.remove('hide');
+      readLessButton.classList.remove('show');
+    }
+  };
+
+  const showMoreArticles = () => {
+    visibleArticles += 2;
+    readMoreButton.innerText = 'loading...';
+    displayArticles();
+    readMoreButton.innerText = 'read more';
+  };
+
+  const showLessArticles = () => {
+    visibleArticles = 4;
+    readMoreButton.innerText = 'loading...';
+    displayArticles();
+    readMoreButton.innerText = 'read more';
+  };
+
+  readMoreButton.addEventListener('click', showMoreArticles);
+  readLessButton.addEventListener('click', showLessArticles);
+
+  fetchMoreArticles();
+};
+
+getArticles();
+
+// event listener
+navIcon.addEventListener('click', showNav);
+window.addEventListener('scroll', scrollUpdate);
+window.addEventListener('scroll', showBg);
